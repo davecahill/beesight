8f9796b1eea336cc25b5aa0e304ad847fed52712
Dave Cahill <dpcahill@gmail.com>
2013-06-12 23:46:30 +0900

    save progress on multiple datapoints version

--- beesight.py
+++ beesight.py
@@ -5,11 +5,12 @@ import urllib2
 import requests
 import sys
 import simplejson
+import pytz
+import time
 
 # complain on config file issues
-
 # complain on bad login
-
+# don't hardcode timezone to japan
 
 CONFIG_FILE_NAME = 'config.ini'
 INSIGHT_SECTION = 'insight'
@@ -17,28 +18,15 @@ BEEMINDER_SECTION = 'beeminder'
 GOAL_NAME = 'meditate'
 BASE_URL= "https://www.beeminder.com/api/v1/"
 
-DATAPOINTS_URL = BASE_URL + "/users/%s/goals/%s/datapoints.json?auth_token=%s"
+GET_DATAPOINTS_URL = BASE_URL + "users/%s/goals/%s/datapoints.json?auth_token=%s"
+POST_MANY_DATAPOINTS_URL = BASE_URL + "users/%s/goals/%s/datapoints/create_all.json?auth_token=%s"
+POST_DATAPOINTS_URL = GET_DATAPOINTS_URL + "&timestamp=%s&value=%s"
+
 
 LOGIN_URL = "https://insighttimer.com/user_session"
 INSIGHT_CSV_URL = "https://insighttimer.com/users/export"
 
 headers = {}
-"""
-headers = {
-"Host": "insighttimer.com",
-"Connection": "keep-alive",
-"Content-Length": "80",
-"Cache-Control": "max-age=0",
-"Origin": "https://insighttimer.com",
-"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.163 Safari/535.19",
-"Content-Type": "application/x-www-form-urlencoded",
-"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-"Referer": "https://insighttimer.com/user_session/new",
-#"Accept-Encoding": "gzip,deflate,sdch",
-"Accept-Language": "en-US,en;q=0.8",
-"Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3"
-}
-"""
 
 def get_insight_data():
         config = ConfigParser.RawConfigParser()
@@ -60,6 +48,29 @@ def get_insight_data():
 
         return r.text.split('\n')
 
+def post_beeminder_data(data):
+        config = ConfigParser.RawConfigParser()
+        config.read(CONFIG_FILE_NAME)
+
+        username = config.get(BEEMINDER_SECTION, "username")
+        auth_token = config.get(BEEMINDER_SECTION, "auth_token")
+
+
+        # json_headers
+        headers = {'content-type': 'application/json'}
+        values = {'datapoints' : data}
+        json_data = simplejson.dumps(values)
+
+        session = requests.session()
+        full_url = POST_MANY_DATAPOINTS_URL % (username, GOAL_NAME, auth_token)
+
+        """
+        r = session.post(full_url, data=json_data) #btry w/o headers, headers=headers)
+        print r.url
+        print r.text
+        """
+        print json_data
+
 def get_beeminder():
         config = ConfigParser.RawConfigParser()
         config.read(CONFIG_FILE_NAME)
@@ -67,26 +78,37 @@ def get_beeminder():
         username = config.get(BEEMINDER_SECTION, "username")
         auth_token = config.get(BEEMINDER_SECTION, "auth_token")
 
-        response = urllib2.urlopen(DATAPOINTS_URL % (username, GOAL_NAME, auth_token))
+        response = urllib2.urlopen(GET_DATAPOINTS_URL % (username, GOAL_NAME, auth_token))
         the_page = response.read()
         return the_page
 
 def beeminder_to_one_per_day(beeminder_output):
     bm = simplejson.loads(beeminder_output)
 
+    print bm
+
     s = {}
 
     # skip first two header lines
     for entry in bm:
-        dt = datetime.date.fromtimestamp(entry['timestamp'])
-        if not dt in s:
-            s[dt] = 1
+        tz = pytz.timezone('Asia/Tokyo')
+        ts = entry['timestamp']
+        print ts
+        dt = datetime.datetime.fromtimestamp(ts)
+        print "bmdt", dt
+        ld = tz.localize(dt)
+        d = ld.date()
+        print "d", d
+
+        if not d in s:
+            s[d] = 1
 
     return s.keys()
 
 
 def csv_to_one_per_day(csv_lines):
 
+    print csv_lines
     s = {}
 
     # skip first two header lines
@@ -103,11 +125,30 @@ def csv_to_one_per_day(csv_lines):
 
     return s.keys()
 
+def date_to_jp_timestamp(dt):
+    tz = pytz.timezone('Asia/Tokyo')
+    d = datetime.datetime.combine(dt, datetime.time())
+    offset = tz.utcoffset(d)
+    ld = d - offset
+    return int(time.mktime(ld.timetuple()))
+
 if __name__ == "__main__":
 
     insight_dates = csv_to_one_per_day(get_insight_data())
     beeminder_dates = beeminder_to_one_per_day(get_beeminder())
 
-    print insight_dates
-    print beeminder_dates
+    print "------"
+    print sorted(insight_dates)
+    print sorted(beeminder_dates)
+    print "------"
+    new_dates = sorted(list(set(insight_dates) - set(beeminder_dates)))
+
+    print new_dates[0]
+    new_datapoints = [{'timestamp': date_to_jp_timestamp(d), 'value':1.0, "comment":"test"} for d in new_dates]
+
+    print new_datapoints
+    print "**** %s to be posted" % len(new_datapoints)
+
+    post_beeminder_data(new_datapoints[0:1])
+    print "posted"
 
